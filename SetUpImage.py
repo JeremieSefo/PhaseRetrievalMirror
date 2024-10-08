0ca7656542966be9c293fe09cc88092e8da21a2d
JeremieSefo <jsefo@uni-muenster.de>
2024-10-08 21:34:18 +0200

    my odl pdhg with support constraint dos not work

--- SetUpImage.py
+++ SetUpImage.py
@@ -2,72 +2,107 @@ import numpy as np
 from PIL import Image
 import skimage as ski
 import matplotlib.pyplot as plt
+import scipy.misc
 
 def get_phantom(dim):
     phantom = ski.img_as_float(ski.data.shepp_logan_phantom())
     return ski.transform.resize(phantom, (dim, dim))
 
-def setUpImage(Nx, Ny):
-    
-    NumPix = 0 + np.floor(0.5 * (1-2**(-1)) *Nx)
-    bord = NumPix/Nx
-    #x_true = im[500:516, 500:516]
-    mask = (0 + 0j) * np.zeros((Nx,Ny))
-    mask[int(bord * Nx):int((1-bord)*Nx),int(bord * Ny):int((1-bord)*Ny)] = ((1)*1 + (0) *1j) * np.ones((int((1-2*bord)*Nx),int((1-2*bord)*Ny)))
-    x_true = (0 + 0j) * np.zeros((Nx,Ny))
+class setUpImage:
+    def __init__(self, Nx, Ny):
+        self.Nx = Nx
+        self.Ny = Ny
 
-    i, k = np.meshgrid(np.arange(int(Nx/2)), np.arange(int(Ny/2)))
-    #omega = np.exp( - 2 * np.pi * 1j /int(Nx/2) * int(Ny/2) ) # (i + k *0j) *  #i+k**2-k*i   #k + i**2 - i*k
-    grd_truths = []
-    x_true[int(bord * Nx):int((1-bord)*Nx),int(bord * Ny):int((1-bord)*Ny)] = ((1)*1 + (1) *1j) * np.ones((int((1-2*bord)*Nx),int((1-2*bord)*Ny))) #(7 + 0j)* np.random.normal(0, 1, size = (int(Nx/2),int(Ny/2))) + (0 + 5j)* np.random.normal(0, 1, size = (int(Nx/2),int(Ny/2))) #(i + k *1j) * np.ones((int(Nx/2),int(Ny/2)))
-    x_true = x_true / np.max(np.abs(x_true))
-    grd_truths.append(x_true)
-    #x_true[1,1] = 2 + 1j
-    #print(x_true)
-    #x_true[0, 1:Ny-1] = np.ones((1,Ny-2))
-    img = Image.open('ISIC_0000004_cancer.jpg')
-    #img = iio.v2.imread('ISIC_0000004_cancer.jpg')
-    #x_true = Image.rgb2gray(img)
-    x_true3 = np.array( img.resize((Nx, Ny)))
-    #x_true = x_true/np.max(np.abs(x_true))
-    x_true = x_true3[:, :,0] + (1j) * x_true3[:, :,2]# np.zeros((Nx,Ny))
-    x_true = x_true / np.max(np.abs(x_true))
-    x_true *= mask 
-    grd_truths.append(x_true)
-    #x_true *= mask 
-    img.save('resized_image.jpg')
-    #print(x_true[0])
-    #print(np.shape(x_true))
-    #x_true = img
-    #plt.imshow(img)
-    img = ski.img_as_float(ski.data.camera())
-    img_res = ski.transform.resize(img, (Nx, Ny))
-    x_true = 2. * img_res + 2.j * (- img_res)
-    x_true = x_true / np.max(np.abs(x_true))
-    
-    #x_true = (1 + 1j) * np.ones(x_true.shape)
-    x_true *= mask 
-    grd_truths.append(x_true)
+    def __call__(self,):
+ 
+        NumPix = 0 + np.floor(0.5 * (1-2**(-1)) *self.Nx)
+        bord = NumPix/self.Nx
+        #x_true = im[500:516, 500:516]
+        mask = (0 + 0j) * np.zeros((self.Nx,self.Ny))
+        mask[int(bord * self.Nx):int((1-bord)*self.Nx),int(bord * self.Ny):int((1-bord)*self.Ny)] = ((1)*1 + (0) *1j) * np.ones((int((1-2*bord)*self.Nx),int((1-2*bord)*self.Ny)))
+        lower = int(bord * self.Nx)
+        upper = int((1-bord)*self.Nx)
 
-    x_true[int(bord * Nx):int((1-bord)*Nx),int(bord * Ny):int((1-bord)*Ny)] = get_phantom(int((1-2*bord)*Nx)) + get_phantom(int((1-2*bord)*Nx)) * 1.j
-    x_true *= mask 
-    grd_truths.append(x_true)
-    '''
-    plt.imshow(x_true.real, cmap='gray')
-    plt.colorbar()
-    '''
-    x_true_real = np.real(x_true)#.reshape((Nx, Ny))
-    x_true_imag = np.imag(x_true)#.reshape((Nx, Ny))
-    #print(x_true_real)
-    #x_true = x_true_real
-    '''
-    fig, ((ax00, ax01, ax02, ax03), (ax10, ax11, ax12, ax13)) = plt.subplots(nrows = 2, ncols=4, figsize=(10,5))
-    
-    ax00.imshow(x_true_imag, cmap='gray') 
-    ax00.set_title("image ")
-    ax00.axis('off')
-    '''
+        x_true = (0 + 0j) * np.zeros((self.Nx,self.Ny))
 
-    return grd_truths, mask
-    ##plt.imshow(-1j * x_true)
-    #plt.colorbar()
+        i, k = np.meshgrid(np.arange(int(self.Nx/2)), np.arange(int(self.Ny/2)))
+        #omega = np.exp( - 2 * np.pi * 1j /int(self.Nx/2) * int(self.Ny/2) ) # (i + k *0j) *  #i+k**2-k*i   #k + i**2 - i*k
+        grd_truths = []
+        x_true[int(bord * self.Nx):int((1-bord)*self.Nx),int(bord * self.Ny):int((1-bord)*self.Ny)] = ((1)*1 + (1) *1j) * np.ones((int((1-2*bord)*self.Nx),int((1-2*bord)*self.Ny))) #(7 + 0j)* np.random.normal(0, 1, size = (int(self.Nx/2),int(self.Ny/2))) + (0 + 5j)* np.random.normal(0, 1, size = (int(self.Nx/2),int(self.Ny/2))) #(i + k *1j) * np.ones((int(self.Nx/2),int(self.Ny/2)))
+        x_true = x_true / np.max(np.abs(x_true))
+        #x_true = np.rot90(x_true, -1)  # Change axis convention
+        x_true = np.rot90(x_true, -1)
+        x_true = np.rot90(x_true, -1)
+        grd_truths.append(x_true)
+        #x_true[1,1] = 2 + 1j
+        #print(x_true)
+        #x_true[0, 1:self.Ny-1] = np.ones((1,self.Ny-2))
+        img = Image.open('ISIC_0000004_cancer.jpg')
+        #img = iio.v2.imread('ISIC_0000004_cancer.jpg')
+        #x_true = Image.rgb2gray(img)
+        x_true3 = np.array( img.resize((self.Nx, self.Ny)))
+        #x_true = x_true/np.max(np.abs(x_true))
+        x_true = x_true3[:, :,0] + (1j) * x_true3[:, :,2]# np.zeros((self.Nx,self.Ny))
+        x_true = x_true / np.max(np.abs(x_true))
+        x_true *= mask 
+        x_true = np.rot90(x_true, -1)
+        x_true = np.rot90(x_true, -1)
+        grd_truths.append(x_true)
+        #x_true *= mask 
+        img.save('resized_image.jpg')
+        #print(x_true[0])
+        #print(np.shape(x_true))
+        #x_true = img
+        #plt.imshow(img)
+        img = ski.img_as_float(ski.data.camera())
+        img_res = ski.transform.resize(img, (self.Nx, self.Ny))
+        x_true = 2. * img_res + 2.j * (- img_res)
+        x_true = x_true / np.max(np.abs(x_true))
+        
+        #x_true = (1 + 1j) * np.ones(x_true.shape)
+        x_true *= mask 
+        x_true = np.rot90(x_true, -1)
+        x_true = np.rot90(x_true, -1)
+        grd_truths.append(x_true)
+
+        x_true[int(bord * self.Nx):int((1-bord)*self.Nx),int(bord * self.Ny):int((1-bord)*self.Ny)] = get_phantom(int((1-2*bord)*self.Nx)) + get_phantom(int((1-2*bord)*self.Nx)) * 1.j
+        x_true *= mask 
+        x_true = np.rot90(x_true, -1)
+        x_true = np.rot90(x_true, -1)
+        grd_truths.append(x_true)
+
+        image = scipy.datasets.ascent().astype('complex').reshape((512, 512)) #resize((int((1-2*bord)*self.Nx)*int((1-2*bord)*self.Ny))) #.
+        image /= image.max()
+        image = np.rot90(image, 1)
+        image = np.rot90(image, 1)
+    
+    # Change axis convention
+        #image /= np.max(np.abs(x_true))
+        x_true = (0 + 0j) * np.zeros((self.Nx,self.Ny))
+
+        x_true[int(bord * self.Nx):int((1-bord)*self.Nx),int(bord * self.Ny):int((1-bord)*self.Ny)] = np.resize(image, (int((1-2*bord)*self.Nx),int((1-2*bord)*self.Ny))) + np.resize(image, (int((1-2*bord)*self.Nx),int((1-2*bord)*self.Ny))) *1.j # image.resize((int((1-2*bord)*self.Nx),int((1-2*bord)*self.Ny))) #+ image.T.copy().resize((int((1-2*bord)*self.Nx),int((1-2*bord)*self.Nx))) * 1.j
+        #x_true = image.T.copy() + image.T.copy() * 1.j
+        #x_true = x_true.resize((self.Nx, self.Ny))
+        #x_true *= mask 
+        grd_truths.append(x_true)
+        '''
+        plt.imshow(x_true.real, cmap='gray')
+        plt.colorbar()
+        '''
+        x_true_real = np.real(x_true)#.reshape((self.Nx, self.Ny))
+        x_true_imag = np.imag(x_true)#.reshape((self.Nx, self.Ny))
+        #print(x_true_real)
+        #x_true = x_true_real
+        '''
+        fig, ((ax00, ax01, ax02, ax03), (ax10, ax11, ax12, ax13)) = plt.subplots(nrows = 2, ncols=4, figsize=(10,5))
+        
+        ax00.imshow(x_true_imag, cmap='gray') 
+        ax00.set_title("image ")
+        ax00.axis('off')
+        '''
+        self.mask = mask
+        self.grd_truths = grd_truths
+        return grd_truths, mask
+        ##plt.imshow(-1j * x_true)
+        #plt.colorbar()    
+
---
+++ TVRegularise.py
@@ -0,0 +1,87 @@
+#matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import odl
+import scipy.misc
+
+
+
+
+class support(odl.solvers.Functional):
+
+    def __init__(self, mask, X):
+        self.mask = mask
+        self.X = X
+        super(support, self).__init__(self.X)
+    def __call__(self, x):
+        M = np.ones(self.mask.shape)
+        M -= self.mask
+        func = odl.solvers.IndicatorZero(self.X)
+        return func(x*M)
+    def proximal(self, x):
+        """Return the proximal factory of the functional.
+
+        This is the zero operator.
+        """
+        def support_proximal(space, out):
+         
+            class support_operator(odl.Operator):
+
+                def __init__(self,):
+                    
+                    super(support_operator, self).__init__(
+                        domain=space, range=space, linear=True)
+                def __call__(self, x, out):
+                    out.assign( x*self.mask)
+                    
+        #     """Proximal factory for zero operator.
+
+        #     Parameters
+        #     ----------
+        #     sigma : positive float, optional
+        #         Step size parameter.
+        #     """
+        #     return support_operator(self.X, self.mask)
+
+        return support_proximal
+
+import SensingMatrix as sm
+
+
+def TVregularize(y, alpha,  mask, x, X):
+    # `min_pt` corresponds to `a`, `max_pt` to `b`
+    #X = odl.uniform_discr(min_pt=[0, 0], max_pt=y.shape, shape=y.shape)
+    #print('Pixel size:', X.cell_sides)
+    grad = odl.Gradient(X)
+    #print('Gradient domain X:', grad.domain)
+    #print('Gradient range X^d:', grad.range)
+    #A = sm.FourierMatrix(mask.shape[0], mask.shape[1])
+    #B = X.element(A.Matrix)
+    #op = odl.MultiplyOperator(B)
+    I = odl.IdentityOperator(X)
+    L = odl.BroadcastOperator(I, grad)
+    #print('L domain X:', L.domain)
+    #print('L range X x X^d:', L.range)
+    # `.translated(y)` takes care of the `. - y` part in the function
+    f_1 = odl.solvers.L2NormSquared(X).translated(y)
+    # The regularization parameter `alpha` is multiplied with the L1 norm.
+    # The L1 norm must be defined on X^d, the range of the gradient.
+    #alpha = 0.15
+    f_2 = alpha * odl.solvers.L1Norm(grad.range)
+    f = odl.solvers.SeparableSum(f_1, f_2)
+
+    # We can test whether everything makes sense by evaluating `f(L(x))`
+    # at some arbitrary `x` in `X`. It should produce a scalar.
+    #print(f(L(X.zero())))
+    g = support(mask, X)
+    
+    #h = odl.solvers.IndicatorNonnegativity(X)
+    #print(g(np.array([2, 0, 0, 0]).reshape(y.shape)))
+    L_norm = 1.1 * odl.power_method_opnorm(L, xstart=y, maxiter=20)
+
+    tau = 1.0 / L_norm
+    sigma = 1.0 / L_norm
+    
+    #print('||L|| =', L_norm)
+    odl.solvers.pdhg(x, g, f, L, tau=tau, sigma=sigma, niter=20)
+    x.show('Denoised image');
